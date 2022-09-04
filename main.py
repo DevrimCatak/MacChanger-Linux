a42c1b71873ad4fcf777ea2a569cb2934e3a96f3
devrimcatak-kali <devrimctk@gmail.com>
2022-09-04 17:29:12 -0400

    completed

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import subprocess
+import optparse
+import re
 
 
-# Press the green button in the gutter to run the script.
+def getInput():
+    parseObject = optparse.OptionParser()
+    parseObject.add_option("-i", "--interface", dest="interface", help="interface to change!")
+    parseObject.add_option("-m", "--mac", dest="macAddress", help="new mac address!")
+    return parseObject.parse_args()
+
+
+def changeMac(interface, macAddress):
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", macAddress])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+def controlChangeMac(interface, macAddress):
+    ifconfig = subprocess.check_output(["ifconfig", interface])
+    newMacAddress = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig))
+
+    if newMacAddress:
+        if newMacAddress.group(0) == macAddress:
+            print("The MAC address has been successfully changed.")
+        else:
+            print("An error was encountered while changing the MAC address.")
+    else:
+        print("An error was encountered while changing the MAC address.")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    (userInputs, args) = getInput()
+    interface = userInputs.interface
+    macAddress = userInputs.macAddress
+    changeMac(interface, macAddress)
+    controlChangeMac(interface, macAddress)
